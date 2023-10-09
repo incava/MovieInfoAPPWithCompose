d3e277082b64a682e82d2743b733740bc04417d3
incava <68988975+incava@users.noreply.github.com>
2023-10-09 15:44:14 +0900

    feat : 색깔과 패딩을 커스텀

---
+++ app/src/main/java/com/incava/movieinfoappwithcompose/ui/theme/Paddings.kt
@@ -0,0 +1,20 @@
+package com.incava.movieinfoappwithcompose.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+
+/**
+ * 패팅 값을 상수로 정의한 오브젝트
+ */
+
+object Paddings {
+    val none = 0.dp
+    val xsamll = 2.dp
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 12.dp
+    val xlarge = 16.dp
+    val extra = 24.dp
+    val xextra = 32.dp
+}
+
